e9385b6023e34ee9e4b393de9c056c9ffbebfac4
abohara706@gmail.com <abohara706@gmail.com>
2020-08-25 20:12:03 -0500

    Initial Commit: demo3 jenkins Practice

---
+++ demo3/src/main/java/com/example/demo3/config/SQSConfiguration.java
@@ -0,0 +1,31 @@
+package com.example.demo3.config;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+public class SQSConfiguration {
+
+    @Bean
+    @Profile("local")
+    public AmazonSQS sqs(@Value("${aws.local.endpoint}") String localEndpoint,@Value("${aws.local.region}") String localRegion){
+        System.out.println("Building sqs....");
+        return AmazonSQSClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(localEndpoint,localRegion))
+                        .build();
+    }
+
+    @Bean
+    @Profile("!local")
+    public AmazonSQS sqs(){
+        return AmazonSQSClientBuilder.standard().build();
+    }
+
+
+
+}
